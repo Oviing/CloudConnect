07b2ad1eb39ab3e3a71c7839deb9ebef005e9f1e
Joel <59056748+Oviing@users.noreply.github.com>
2022-11-07 13:42:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 setup(
-    name='cloudConnect',
+    name='cloudconnect',
     packages=find_packages(include=['connector']),
     version='0.0.1',
     description='A package to connect retrieve the API credentials in SAP BTP',
